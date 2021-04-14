190cf51fa2b54df4d1f3c3fafd78384ff66926bd
Vladimir <vladimir@rupor.agency>
2021-04-14 17:39:19 +0300

    done work

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,67 +1,82 @@
 let lastOperand = 0;
 let operation = null;
-
+let lastNum = null;
 const inputWindow = document.getElementById('inputWindow');
+const num = document.querySelectorAll('.num');
+
+
 
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
+    num.forEach(num => num.removeAttribute("disabled"))
 })
 
 document.getElementById('btn_1').addEventListener('click', function () {
     inputWindow.value += '1';
+    lastNum = this.value;
 })
 document.getElementById('btn_2').addEventListener('click', function () {
     inputWindow.value += '2';
+    lastNum = this.value;
 })
 document.getElementById('btn_3').addEventListener('click', function () {
     inputWindow.value += '3';
+    lastNum = this.value;
 })
 document.getElementById('btn_4').addEventListener('click', function () {
     inputWindow.value += '4';
+    lastNum = this.value;
 })
 document.getElementById('btn_5').addEventListener('click', function () {
     inputWindow.value += '5';
+    lastNum = this.value;
 })
 document.getElementById('btn_6').addEventListener('click', function () {
     inputWindow.value += '6';
+    lastNum = this.value;
 })
 document.getElementById('btn_7').addEventListener('click', function () {
     inputWindow.value += '7';
+    lastNum = this.value;
 })
 document.getElementById('btn_8').addEventListener('click', function () {
     inputWindow.value += '8';
+    lastNum = this.value;
 })
 document.getElementById('btn_9').addEventListener('click', function () {
     inputWindow.value += '9';
+    lastNum = this.value;
 })
 document.getElementById('btn_0').addEventListener('click', function () {
     inputWindow.value += '0';
+    lastNum = this.value;
 })
 
 document.getElementById('btn_sum').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
     operation = 'sum';
-    inputWindow.value = '';
+    inputWindow.value += '+';
+    
 })
 document.getElementById('btn_def').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
     operation = 'def';
-    inputWindow.value = '';
+    inputWindow.value += '-';
 })
 
 document.getElementById('btn_mult').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
     operation = 'mult';
-    inputWindow.value = '';
+    inputWindow.value += '*';
 })
 
 document.getElementById('btn_share').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
     operation = 'share';
-    inputWindow.value = '';
+    inputWindow.value += '/';
 })
 document.getElementById('btn_sqrt').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
@@ -73,23 +88,36 @@ document.getElementById('btn_sqrt').addEventListener('click', function () {
     inputWindow.value = result;
 })
 document.getElementById('btn_calc').addEventListener('click', function () {
+    console.log(lastOperand);
     if(operation === 'sum'){
-        const result = lastOperand + parseInt(inputWindow.value);
+        const operand = String(inputWindow.value);
+        console.log(inputWindow.value);
+        const result = lastOperand + parseInt(lastNum);
         operation = null;
         lastOperand = 0;
-        inputWindow.value = result;
+        inputWindow.value = operand + '=' + result;
     }
     if(operation === 'def'){
-        const result = lastOperand - parseInt(inputWindow.value);
+        const operand = String(inputWindow.value)
+        const result = lastOperand - parseInt(lastNum);
         operation = null;
         lastOperand = 0;
-        inputWindow.value = result;
+        inputWindow.value = operand + '=' + result;
     }
     if(operation === 'mult'){
-        const result = lastOperand * parseInt(inputWindow.value);
+        const operand = String(inputWindow.value)
+        const result = lastOperand * parseInt(lastNum);
         operation = null;
         lastOperand = 0;
-        inputWindow.value = result;
+        inputWindow.value = operand + '=' + result;
     }
+    if(operation === 'share'){
+        const operand = String(inputWindow.value)
+        const result = lastOperand / parseInt(lastNum);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = operand + '=' + result;
+    }
+    num.forEach(num => num.setAttribute("disabled", "disabled"))
 
 })
