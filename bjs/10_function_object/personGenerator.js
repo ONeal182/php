427b3321d4d60a1510636376110dc6ca733a58e8
ONeal <kigat63@gmail.com>
2021-05-05 14:56:37 +0400

    done step 3

--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -119,13 +119,21 @@ const personGenerator = {
             return Math.random() * (max - min) + min;
         }
         var date1 = date1 || '01-01-1970'
-        var date2 = date2 || new Date().toLocaleDateString()
+        var date2 = date2 || new Date().toLocaleDateString();
         date1 = new Date(date1).getTime()
         date2 = new Date(date2).getTime()
         if (date1 > date2) {
-            return new Date(getRandomArbitrary(date2, date1)).toLocaleDateString()
+            return new Date(getRandomArbitrary(date2, date1)).toLocaleDateString('ru', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            })
         } else {
-            return new Date(getRandomArbitrary(date1, date2)).toLocaleDateString()
+            return new Date(getRandomArbitrary(date1, date2)).toLocaleDateString('ru', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            })
 
         }
     },
